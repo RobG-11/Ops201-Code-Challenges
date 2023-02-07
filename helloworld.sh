7f786fd3f98391c71511054d0ab7824d274cd253
user <robert.gregor11@gmail.com>
2023-02-07 20:50:56 +0000

    071450FFEB23

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: 02 - My First Bash Script
+# Author: Robert Gregor
+# Date of latest revision: 071429FFEB23
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo 'I am working perfectly'
+
+# End
