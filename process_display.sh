faa9aacd3cf91c456e625813de0d3baf717a0dc5
user <robert.gregor11@gmail.com>
2023-02-11 00:35:26 +0000

    x

---
+++ process_display.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script: 05 - Loops
+# Author: Robert Gregor
+# Date of latest revision: 101500FFEB23
+# Purpose:
+    # Write a script that displays running processes, asks the user for a PID, then kills the process with that PID
+    # Use a loop in your script, will facilitate menu system of script, so it can prompt user to choose option
+# Code Fellows Resources:
+    # [Loops!] - (https://ryanstutorials.net/bash-scripting-tutorial/bash-loops.php)
+    # [bash for loop] -(https://codefellows.github.io/ops-201-guide/curriculum/class-05/challenges/demo/bash.html)
+    # [PowerShell Tutorial] - (https://codefellows.github.io/ops-201-guide/curriculum/class-03/challenges/demo/powershell.html)
+    # [Z shell Tutorial] - (https://codefellows.github.io/ops-201-guide/curriculum/class-03/challenges/demo/zsh.html)
+# My Sources:
+
+# Main
+
+
+# End
+
