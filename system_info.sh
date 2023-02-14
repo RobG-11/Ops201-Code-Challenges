587a7c4c6731e5a2cca1a3772a87b408ed83b40d
user <robert.gregor11@gmail.com>
2023-02-14 18:26:57 +0000

    update

--- system_info.sh
+++ system_info.sh
@@ -21,16 +21,17 @@
 # My Sources:
     # [how to grep and print the next N lines after the hit?] (https://superuser.com/questions/298123/how-to-grep-and-print-the-next-n-lines-after-the-hit)
     # [How to Grep Multiple Patterns – Syntax] (https://phoenixnap.com/kb/grep-multiple-strings)
+    # [lshw command in Linux with Examples] (https://www.geeksforgeeks.org/lshw-command-in-linux-with-examples/)
+    # [Sed Command to Delete a Line] (https://linuxhint.com/sed-command-to-delete-a-line/)
 
 # Main
 
 clear
 echo "";
+echo "The name of this computer is '"$(hostname)"'";
 
 while true
 do
-        echo "";
-        echo "The name of this computer is '"$(hostname)"'";
         echo "";
         echo "Please choose a system component to examine:";
 
@@ -46,19 +47,16 @@ do
 
         if [[ $system_component == "exit" ]]; then
             echo "system_info.sh exited successfully!"
+            echo "";
             exit
         elif [[ $system_component == "1" ]]; then
-            sudo lshw -C cpu | head -n 7
-            echo ""
+            sudo lshw -C cpu | head -n 7 | sed '/bus info/{n;d;}'
         elif [[ $system_component == "2" ]]; then
             sudo lshw -C memory | tail -n 4
-            echo ""
         elif [[ $system_component == "3" ]]; then
-            sudo lshw -C display
-            echo ""
+            sudo lshw -C display | sed '/bus info/{n;d;}' | sed '/bus info/{n;d;}'
         elif [[ $system_component == "4" ]]; then
             sudo lshw -C network
-            echo ""
         else
             echo "Invalid Selection Input! Try Again!"
         fi
