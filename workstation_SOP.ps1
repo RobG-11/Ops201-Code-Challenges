cf0885cd5221cab250f11443715c53abd1ba06b4
user <robert.gregor11@gmail.com>
2023-02-16 21:57:20 +0000

    a

--- workstation_SOP.ps1
+++ workstation_SOP.ps1
@@ -2,7 +2,7 @@
     # [How to enable Remote Desktop from PowerShell on Windows 10] (https://pureinfotech.com/enable-remote-desktop-powershell-windows-10/)
     # [Create a User Account in Windows 10 with PowerShell] (https://winaero.com/create-user-account-windows-10-powershell/)
     # [Get-Date] (https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7.3)
-    # 
+    # [How to enable execution of PowerShell scripts?] (https://superuser.com/questions/106360/how-to-enable-execution-of-powershell-scripts)
 
 # Main
 
@@ -14,7 +14,6 @@ function Get-RdpNetAuthEnabled {
 }
 
 # Function takes user full name & username as input, creates user with no current password, prints new user into to screen to verify
-runas /noprofile /user:Admin
 
 function Get-NewUser {
     $user_full_name = Read-Host "Please enter new users full name"
@@ -22,7 +21,8 @@ function Get-NewUser {
     $password = Read-Host -AsSecureString "Please enter a secure password"
     $current_date = Get-Date -DisplayHint Date
     New-LocalUser -Name "$user_name" -Password $password -FullName $user_full_name -Description "Created $current_date"
-    Get-LocalUser -Name $user_name
+    $verify = Get-LocalUser -Name $user_name
+    "$verify"
 }
 
 Get-NewUser
