edecb8d0638ef1616d85b2f6ee96f886aad2ec80
user <robert.gregor11@gmail.com>
2023-02-16 22:19:51 +0000

    a

--- workstation_SOP.ps1
+++ workstation_SOP.ps1
@@ -3,16 +3,10 @@
     # [Create a User Account in Windows 10 with PowerShell] (https://winaero.com/create-user-account-windows-10-powershell/)
     # [Get-Date] (https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date?view=powershell-7.3)
     # [How to enable execution of PowerShell scripts?] (https://superuser.com/questions/106360/how-to-enable-execution-of-powershell-scripts)
+    # [add "Remote Desktop User" powershell] (https://learn.microsoft.com/en-us/answers/questions/728543/add-remote-desktop-user-powershell)
+    # 
 
 # Main
-
-# Function enables RDP connections, enables Network Level Authentication, enables Windows firewall rules to allow incoming RDP, enables RDP for user
-function Get-RdpNetAuthEnabled {
-    Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\‘ -Name “fDenyTSConnections” -Value 0
-    Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1
-    Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
-    -Group "Remote Desktop Users" -Member $user_name
-}
 
 # Function takes user full name & username as input, creates user with no current password, prints new user into to screen to verify
 
@@ -29,13 +23,18 @@ Write-Output "Welcome to the new user account set up program!"
 
 Get-NewUser
 
+
+# Function enables RDP connections, enables Network Level Authentication, enables Windows firewall rules to allow incoming RDP, enables RDP for user
+function Get-RdpNetAuthEnabled {
+    Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\‘ -Name “fDenyTSConnections” -Value 0
+    Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1
+    Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
+}
+
 $enable_rdp = Read-Host "Would you like to enable RDP for this account (Y/N)?"
 
 if ($enable_rdp = "Y") {
     Get-RdpNetAuthEnabled
 }
-
-
-    
 
 # End
