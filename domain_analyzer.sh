9a84f3cf42ef7e4e887eb704ec5651fc7201b368
user <robert.gregor11@gmail.com>
2023-02-22 23:47:55 +0000

    code-13v1

---
+++ domain_analyzer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script: 13 - Domain Analyzer
+# Author: Robert Gregor
+# Date of latest revision: 221022FFEB23
+# Purpose:
+    # Create a script that asks a user to type a domain, then displays information about the typed domain
+    # Operations that must be used include (whois, dig, host, nslookup)
+# Code Fellows Resources:
+    # [How to Use the whois Command on Linux] (https://www.howtogeek.com/680086/how-to-use-the-whois-command-on-linux/)
+# My Sources:
+    # 
+
+# Main
+
+clear
+
+
+
+# End
