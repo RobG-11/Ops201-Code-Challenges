bf58c17849fb9ce867212fdbf604b7a6a892146f
user <robert.gregor11@gmail.com>
2023-02-23 18:06:23 +0000

    code-13v3

--- test_file.sh
+++ test_file.sh
@@ -2,31 +2,51 @@
 
 # Main
 
+exe_command() {
+    if [[ $user_command == "1" ]]; then
+        whois $user_domain
+        echo ""
+
+    elif [[ $user_command == "2" ]]; then
+        dig $user_domain
+        echo ""
+
+    elif [[ $user_command == "3" ]]; then
+        host $user_domain
+        echo ""
+
+    elif [[ $user_command == "4" ]]; then
+        nslookup $user_domain
+        echo ""
+
+    elif [[ $user_command == "exit" ]]; then
+        echo "";
+        echo "domain_analyser.sh exited successfully!"
+        echo "";
+        exit
+
+    else
+        echo "Invalid command selected!"
+        echo ""
+    fi
+}
+    
 
 while true
 do
-    current_user= whoami
-    if [[ $current_user == "root" ]]; then
-        ps -aux
-
-    else
-        ps -aux | grep --color ^$(whoami)
-    fi
-
+    echo "Please enter the domain you would like to analyze:"
+    read user_domain
     echo ""
-    echo "Process list above includes only the processes you are authorized to kill"
-    echo "Enter PID to Kill Process YOU are running! Type 'exit' to exit program"
-    
-    read kill_this_pid
+    echo "1) whois - provides listing of records containing details about ownership of domains and owners"
+    echo "2} dig - query DNS servers for info regarding various DNS records (host address, mail exchanges, name servers, related info"
+    echo "3) host - used to find IP address of particular domain name "
+    echo "4) nslookup - used to query DNS to obtain domain name or IP address mapping or any other specific DNS record"
+    echo "5) exit - exits program"
+    echo ""
+    echo "Please choose the command you would like to run on your selected domain"
+    read user_command
 
-    if [[ $kill_this_pid == "exit" ]]; then
-        echo "";
-        echo "process_killer.sh exited successfully!"
-        echo "";
-        exit
-        fi
-
-    kill $kill_this_pid
+    exe_command
 done
 
 # End
